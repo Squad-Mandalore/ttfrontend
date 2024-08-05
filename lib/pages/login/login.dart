8b9d734d92f8f1c4c919c56d8efd72dd8c60260a
Mortz <123571236+DrunkWindow@users.noreply.github.com>
2024-08-05 17:40:08 +0200

    feat: SQUAD-11 - adjusted code for IPhone SE

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -47,13 +47,15 @@ class _LoginPageState extends State<LoginPage> {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
 
+    // Determine if the header should be shown based on the height
+    bool shouldShowHeader = _showHeader && height >= 800;
+
     return Scaffold(
       body: SingleChildScrollView(
         physics: NeverScrollableScrollPhysics(),
         child: Container(
           height: height,
           width: width,
-
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage('lib/assets/images/loginPage_logo.png'),
@@ -61,42 +63,53 @@ class _LoginPageState extends State<LoginPage> {
               alignment: Alignment.bottomRight,
             ),
           ),
-          child: AnimatedContainer(
-            duration: Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
-            transform: Matrix4.translationValues(0, _showHeader ? 0 : -200, 0),
-            child: Column(
-              children: <Widget>[
-                AnimatedOpacity(
-                  opacity: _showHeader ? 1.0 : 0.0,
-                  duration: Duration(milliseconds: 300),
-                  child: const Header(),
-                ),
-                const SizedBox(height: 30),
-                EmailInput(
-                  focusNode: _emailFocusNode,
-                ),
-                const SizedBox(height: 30),
-                PasswordInput(
-                  focusNode: _passwordFocusNode,
-                ),
-                const SizedBox(height: 50),
-                LoginButton(
-                  onPressed: () {
-                    // login logic
-                  },
-                ),
-                const SizedBox(height: 20),
-                CustomDivider(),
-                const SizedBox(height: 20),
-                RegisterButton(
-                  onPressed: () {
-                    // registration logic
-                  },
-                ),
-                SizedBox(height: height * 0.10),
-              ],
-            ),
+          child: Column(
+            children: <Widget>[
+              // Space between top and first text
+              SizedBox(height: shouldShowHeader ? 0 : 100), // Adjust as needed
+
+              // Header with animated opacity
+              AnimatedOpacity(
+                opacity: shouldShowHeader ? 1.0 : 0.0,
+                duration: Duration(milliseconds: 300),
+                child: shouldShowHeader ? const Header() : SizedBox.shrink(),
+              ),
+
+              // Adjust the spacing based on header presence
+              SizedBox(height: shouldShowHeader ? 30 : 0),
+
+              // Email Input Field
+              EmailInput(
+                focusNode: _emailFocusNode,
+              ),
+              const SizedBox(height: 30),
+
+              // Password Input Field
+              PasswordInput(
+                focusNode: _passwordFocusNode,
+              ),
+              const SizedBox(height: 50),
+
+              // Login Button
+              LoginButton(
+                onPressed: () {
+                  // login logic
+                },
+              ),
+              const SizedBox(height: 20),
+
+              // Custom Divider
+              CustomDivider(),
+              const SizedBox(height: 20),
+
+              // Register Button
+              RegisterButton(
+                onPressed: () {
+                  // registration logic
+                },
+              ),
+              SizedBox(height: height * 0.10),
+            ],
           ),
         ),
       ),
