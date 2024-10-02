7f2706b138558b71f84a1751d8807892ab8a4255
MrLappes <79363858+MrLappes@users.noreply.github.com>
2024-10-02 10:26:24 +0200

    feat: SQUAD-32 Fixed that b*

--- lib/pages/overview/utils/pdf_generation.dart
+++ lib/pages/overview/utils/pdf_generation.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:pdfx/pdfx.dart';
+import 'package:pdfx/pdfx.dart' as pdfx;
 import 'package:path_provider/path_provider.dart';
 import 'package:ttfrontend/modules/widgets/custom_popup.dart';
 import 'package:ttfrontend/service/api_service.dart';
 import 'package:ttfrontend/service/models/graphql_query.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show Uint8List, kIsWeb;
 import 'package:permission_handler/permission_handler.dart';
 import 'package:printing/printing.dart';
 import 'package:pdf/pdf.dart';
@@ -66,25 +67,28 @@ Future<bool> requestStoragePermission(BuildContext context) async {
 }
 
 Future<Directory?> getAppropriateDirectory(BuildContext context) async {
-  bool hasPermission = await requestStoragePermission(context);
-  if (!hasPermission) {
-    throw Exception("Fehlende Berechtigungen um auf den Speicher zuzugreifen");
-  }
+  // Try to get a directory without requiring permissions
+  Directory? directory;
+
   if (kIsWeb || Platform.isIOS) {
-    // Web doesn't support file system access in the same way, IOS sucks
+    // Web and iOS may not support file system access
     return null;
   }
 
   if (Platform.isAndroid) {
-    // For mobile platforms, use external storage or documents directory
-    return await getExternalStorageDirectory();
+    // Use application documents directory
+    directory = await getExternalStorageDirectory();
   } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     // For desktop platforms, use downloads directory
-    return await getDownloadsDirectory();
+    directory = await getDownloadsDirectory();
   }
 
-  return null;
+  // If directory is null or inaccessible, fallback to temporary directory
+  directory ??= await getTemporaryDirectory();
+
+  return directory;
 }
+
 
 Future<void> fetchAndSavePdf(
     String month, String headerColor, BuildContext context) async {
@@ -106,47 +110,59 @@ Future<void> fetchAndSavePdf(
       query: query,
       variables: {
         'month': month,
-        'header_color': formattedHeaderColor, // Pass enum value correctly
+        'header_color': formattedHeaderColor,
       },
     );
 
     final response = await apiService.graphQLRequest(graphQLQuery);
 
-    final base64Pdf = response
-        .data?['generatePdf']; // Access the correct path in the response
+    final base64Pdf = response.data?['generatePdf'];
 
     if (base64Pdf != null) {
       // Decode the Base64 string to get the binary data
       final pdfBytes = base64Decode(base64Pdf);
-      
-      // Get the directory to save the file in the Downloads folder
-      final directory = await getAppropriateDirectory(context);
-      if (directory == null) {
-        throw Exception("Downloads-Ordner konnte nicht geöffnet werden.");
+
+      bool saveSuccess = false;
+      String? filePath;
+
+      try {
+        // Try to get the directory to save the file
+        final directory = await getAppropriateDirectory(context);
+        if (directory == null) {
+          throw Exception("Downloads-Ordner konnte nicht geöffnet werden.");
+        }
+
+        filePath = '${directory.path}/monatsbericht_$month.pdf';
+
+        // Save the PDF file
+        final file = File(filePath);
+        await file.writeAsBytes(pdfBytes);
+
+        final prefs = await SharedPreferences.getInstance();
+        final now = DateTime.now();
+        final formattedDateTime =
+            '${now.day.toString().padLeft(2, '0')}.${now.month.toString().padLeft(2, '0')}.${now.year} ${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}';
+        await prefs.setString('lastPdfRequest', formattedDateTime);
+
+        saveSuccess = true;
+      } catch (e) {
+        // Saving failed, proceed to display the PDF from bytes
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Speichern fehlgeschlagen: $e')),
+          );
+        }
       }
-
-      final filePath = '${directory.path}/monatsbericht_$month.pdf';
-
-      // Save the PDF file
-      final file = File(filePath);
-      await file.writeAsBytes(pdfBytes);
-
-      print('PDF saved to $filePath');
-      bool exists = await file.exists();
-      print('File exists: $exists');
-
-      final prefs = await SharedPreferences.getInstance();
-      final now = DateTime.now();
-      final formattedDateTime =
-          '${now.day.toString().padLeft(2, '0')}.${now.month.toString().padLeft(2, '0')}.${now.year} ${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}';
-      await prefs.setString('lastPdfRequest', formattedDateTime);
 
       // Show the PDF file using a PDF viewer
       if (context.mounted) {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => PdfViewerPage(filePath: filePath),
+            builder: (context) => PdfViewerPage(
+              pdfBytes: pdfBytes,
+              filePath: saveSuccess ? filePath : null,
+            ),
           ),
         );
       }
@@ -154,7 +170,7 @@ Future<void> fetchAndSavePdf(
       throw Exception('No PDF data returned from GraphQL query.');
     }
   } catch (e) {
-    print('Failed to fetch PDF: $e');
+    print('Failed to fetch and save PDF: $e');
     if (context.mounted) {
       GenericPopup.showErrorPopup(
           context, 'Es gab ein Problem beim Herunterladen der PDF-Datei.');
@@ -163,15 +179,48 @@ Future<void> fetchAndSavePdf(
 }
 
 class PdfViewerPage extends StatefulWidget {
-  final String filePath;
+  final Uint8List pdfBytes;
+  final String? filePath; // Optional, in case the file was saved successfully
 
-  const PdfViewerPage({Key? key, required this.filePath}) : super(key: key);
+  const PdfViewerPage({super.key, required this.pdfBytes, this.filePath});
 
   @override
-  _PdfViewerPageState createState() => _PdfViewerPageState();
+  PdfViewerPageState createState() => PdfViewerPageState();
 }
 
-class _PdfViewerPageState extends State<PdfViewerPage> {
+class PdfViewerPageState extends State<PdfViewerPage> {
+  late PdfControllerPinch _pdfController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pdfController = PdfControllerPinch(
+      document: pdfx.PdfDocument.openData(widget.pdfBytes),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pdfController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _printPdf() async {
+    try {
+      await Printing.layoutPdf(
+        onLayout: (PdfPageFormat format) async => widget.pdfBytes,
+        name: 'Monatsbericht${DateTime.now().toIso8601String()}.pdf',
+        format: PdfPageFormat.a4,
+      );
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Fehler beim Drucken der PDF.')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -180,52 +229,12 @@ class _PdfViewerPageState extends State<PdfViewerPage> {
         actions: [
           IconButton(
             icon: const Icon(Icons.print),
-            onPressed: () async {
-              try {
-                final file = File(widget.filePath);
-                final bytes = await file.readAsBytes();
-
-                await Printing.layoutPdf(
-                  onLayout: (PdfPageFormat format) async => bytes,
-                  name: 'Monatsbericht${DateTime.now().toIso8601String()}.pdf',
-                  format: PdfPageFormat.a4,
-                  // You can add more options here
-                );
-              } catch (e) {
-                print('Error printing PDF: $e');
-                if (context.mounted) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text('Fehler beim Drucken der PDF.')),
-                  );
-                }
-              }
-            },
+            onPressed: _printPdf,
           ),
         ],
       ),
-      body: PDFView(
-        filePath: widget.filePath,
-        enableSwipe: true,
-        swipeHorizontal: false,
-        autoSpacing: false,
-        pageFling: false,
-        onError: (error) {
-          print(error.toString());
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Fehler beim Anzeigen der PDF.')),
-            );
-          }
-        },
-        onPageError: (page, error) {
-          print('$page: ${error.toString()}');
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Fehler beim Anzeigen der PDF.')),
-            );
-          }
-        },
+      body: PdfViewPinch(
+        controller: _pdfController,
       ),
     );
   }
