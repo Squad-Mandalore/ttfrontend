35b9e3d4261d61dabde2749d7281f221bbefb7aa
MrLappes <79363858+MrLappes@users.noreply.github.com>
2024-10-30 15:17:25 +0100

    fix: colors once more

--- lib/modules/widgets/custom_popup.dart
+++ lib/modules/widgets/custom_popup.dart
@@ -140,48 +140,56 @@ class GenericPopup extends StatelessWidget {
   }
 
   static void showErrorPopup(BuildContext context, String errorMessage) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return GenericPopup(
-            title: 'Error',
-            agreeText: 'ok',
-            content: Column(
-              children: [
-                const SizedBox(height: 16.0),
-                Text(errorMessage),
-                const SizedBox(height: 16.0),
-              ],
-            ),
-            mode: PopUpMode.error,
-            onAgree: () {
-              Navigator.of(context).pop();
-            },
-          );
-        },
-      );
-    }
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return GenericPopup(
+          title: 'Error',
+          agreeText: 'ok',
+          content: Column(
+            children: [
+              const SizedBox(height: 16.0),
+              Text(errorMessage),
+              const SizedBox(height: 16.0),
+            ],
+          ),
+          mode: PopUpMode.error,
+          onAgree: () {
+            Navigator.of(context).pop();
+          },
+        );
+      },
+    );
+  }
 
-  static void showWarningPopup(BuildContext context, String warningMessage, String title) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return GenericPopup(
-            title: title,
-            agreeText: 'ok',
-            content: Column(
-              children: [
-                const SizedBox(height: 16.0),
-                Text(warningMessage),
-                const SizedBox(height: 16.0),
-              ],
-            ),
-            mode: PopUpMode.warning,
-            onAgree: () {
-              Navigator.of(context).pop();
-            },
-          );
-        },
-      );
-    }
+  static void showWarningPopup(
+      BuildContext context, String warningMessage, String title) {
+    final theme = Theme.of(context);
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return GenericPopup(
+          title: title,
+          agreeText: 'ok',
+          content: Column(
+            children: [
+              const SizedBox(height: 16.0),
+              Text(
+                warningMessage,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: theme.colorScheme.onSurface,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+            ],
+          ),
+          mode: PopUpMode.warning,
+          onAgree: () {
+            Navigator.of(context).pop();
+          },
+        );
+      },
+    );
+  }
 }
